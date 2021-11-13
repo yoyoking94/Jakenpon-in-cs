1135e25d7d5d9956f07bd7288d28a0be3ea07337
yoyoking94 <56436435+yoyoking94@users.noreply.github.com>
2021-11-13 17:51:45 +0100

    Jakenpon in c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Jakenpon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rockButton = new System.Windows.Forms.Button();
+            this.paperButton = new System.Windows.Forms.Button();
+            this.scissorsButton = new System.Windows.Forms.Button();
+            this.lizardButton = new System.Windows.Forms.Button();
+            this.spokeButton = new System.Windows.Forms.Button();
+            this.resField = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rockButton
+            // 
+            this.rockButton.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rockButton.Location = new System.Drawing.Point(214, 163);
+            this.rockButton.Name = "rockButton";
+            this.rockButton.Size = new System.Drawing.Size(100, 100);
+            this.rockButton.TabIndex = 0;
+            this.rockButton.Text = "Rock";
+            this.rockButton.UseVisualStyleBackColor = true;
+            this.rockButton.Click += new System.EventHandler(this.rockButton_Click);
+            // 
+            // paperButton
+            // 
+            this.paperButton.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paperButton.Location = new System.Drawing.Point(320, 163);
+            this.paperButton.Name = "paperButton";
+            this.paperButton.Size = new System.Drawing.Size(100, 100);
+            this.paperButton.TabIndex = 1;
+            this.paperButton.Text = "Paper";
+            this.paperButton.UseVisualStyleBackColor = true;
+            this.paperButton.Click += new System.EventHandler(this.paperButton_Click);
+            // 
+            // scissorsButton
+            // 
+            this.scissorsButton.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scissorsButton.Location = new System.Drawing.Point(426, 163);
+            this.scissorsButton.Name = "scissorsButton";
+            this.scissorsButton.Size = new System.Drawing.Size(100, 100);
+            this.scissorsButton.TabIndex = 2;
+            this.scissorsButton.Text = "Scissors";
+            this.scissorsButton.UseVisualStyleBackColor = true;
+            this.scissorsButton.Click += new System.EventHandler(this.scissorsButton_Click);
+            // 
+            // lizardButton
+            // 
+            this.lizardButton.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lizardButton.Location = new System.Drawing.Point(269, 269);
+            this.lizardButton.Name = "lizardButton";
+            this.lizardButton.Size = new System.Drawing.Size(100, 100);
+            this.lizardButton.TabIndex = 3;
+            this.lizardButton.Text = "Lizard";
+            this.lizardButton.UseVisualStyleBackColor = true;
+            this.lizardButton.Click += new System.EventHandler(this.lizardButton_Click);
+            // 
+            // spokeButton
+            // 
+            this.spokeButton.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spokeButton.Location = new System.Drawing.Point(375, 269);
+            this.spokeButton.Name = "spokeButton";
+            this.spokeButton.Size = new System.Drawing.Size(100, 100);
+            this.spokeButton.TabIndex = 4;
+            this.spokeButton.Text = "Spoke";
+            this.spokeButton.UseVisualStyleBackColor = true;
+            this.spokeButton.Click += new System.EventHandler(this.spokeButton_Click);
+            // 
+            // resField
+            // 
+            this.resField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resField.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resField.ForeColor = System.Drawing.Color.White;
+            this.resField.Location = new System.Drawing.Point(205, 99);
+            this.resField.Name = "resField";
+            this.resField.Size = new System.Drawing.Size(321, 50);
+            this.resField.TabIndex = 5;
+            this.resField.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(746, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(746, 435);
+            this.Controls.Add(this.resField);
+            this.Controls.Add(this.spokeButton);
+            this.Controls.Add(this.lizardButton);
+            this.Controls.Add(this.scissorsButton);
+            this.Controls.Add(this.paperButton);
+            this.Controls.Add(this.rockButton);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jakenpon";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rockButton;
+        private System.Windows.Forms.Button paperButton;
+        private System.Windows.Forms.Button scissorsButton;
+        private System.Windows.Forms.Button lizardButton;
+        private System.Windows.Forms.Button spokeButton;
+        private System.Windows.Forms.Label resField;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jakenpon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rockButton_Click(object sender, EventArgs e)
+        {
+            resField.Text = rockButton.Text;
+            verif();
+        }
+
+        private void paperButton_Click(object sender, EventArgs e)
+        {
+            resField.Text = paperButton.Text;
+            verif();
+        }
+
+        private void scissorsButton_Click(object sender, EventArgs e)
+        {
+            resField.Text = scissorsButton.Text;
+            verif();
+        }
+
+        private void lizardButton_Click(object sender, EventArgs e)
+        {
+            resField.Text = lizardButton.Text;
+            verif();
+        }
+
+        private void spokeButton_Click(object sender, EventArgs e)
+        {
+            resField.Text = spokeButton.Text;
+            verif();
+        }
+
+
+
+        private void verif()
+        {
+            var random = new Random();
+            var list = new List<string> { "Rock", "Paper", "Scissors", "Lezard", "Spoke" };
+            int index = random.Next(list.Count);
+            String listIndex = list[index];
+
+            if (resField.Text == list[index])
+            {
+                resField.Text = "égalité ! we both choose : " + resField.Text;
+            } else if((resField.Text == "Spoke") && (listIndex == "Scissors") || (resField.Text == "Scissors") && (listIndex == "Paper") 
+                    || (resField.Text == "Paper") && (listIndex == "Rock") || (resField.Text == "Rock") && (listIndex == "Lezard")
+                    || (resField.Text == "Lezard") && (listIndex == "Spoke") || (resField.Text == "Spoke") && (listIndex == "Rock")
+                    || (resField.Text == "Rock") && (listIndex == "Scissors") || (resField.Text == "Scissors") && (listIndex == "Rock")
+                    || (resField.Text == "Lezard") && (listIndex == "Paper") || (resField.Text == "Paper") && (listIndex == "Spoke"))
+            {
+                resField.Text = "You win ! you choose : " + resField.Text + "and i choose : " + listIndex;
+            }
+            else
+            {
+                resField.Text = "You loose ! you choose : " + resField.Text + "and i choose : " + listIndex;
+            }
+        }
+
+        //Menu strip
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();         // exit program
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Copyright 2021 © Yovish MOONESAMY\n" + "Developped in 2021\n" + "Why ? because i'm bored\n");
+            // print message through a box
+        }
+    }
+}
